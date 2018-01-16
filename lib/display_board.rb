3eea7c17e4a4150869b888b52e40d06a3c8da346
Hiroki Kato <hkato@sas.upenn.edu>
2018-01-16 21:38:29 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-x = [" "," "," "," "," "," "," "," "," "]
+board = [" "," "," "," "," "," "," "," "," "]
 
-def display_board(x)
+def display_board(board)
 
   puts " #{board(0)} | #{board(1)} | #{board(2)} "
   puts "-----------"
